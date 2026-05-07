f04059901667d0339808b12004331de7a9da9e0b
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-05-07 13:51:03 +0200

    feat: First Version Habbit Logger

---
+++ silvermax.Habbit-Logger/Habbit.cs
@@ -0,0 +1,10 @@
+﻿namespace Habbit_Logger
+{
+    internal class Habbit
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime Date { get; set; }
+        public string? Tracker { get; set; }
+    }
+}
